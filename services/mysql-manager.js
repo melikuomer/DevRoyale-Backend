b0b77c5a3f7e9105881678f0a5939c4ee6d43ce8
melikuomer <meliksahuomer@gmail.com>
2022-12-03 16:10:40 +0300

    Fixed DB Connection

--- services/mysql-manager.js
+++ services/mysql-manager.js
@@ -1,21 +1,17 @@
-const mysql = require('mysql')
+
+const mysql = require('mysql2');
 
 const database = require("../config.json").database;
-
 
 const connection = mysql.createConnection({
     host : database.ip,
     user : database.username,
     password: database.password,
     database: database.database_name
-
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
 
-    console.log('MySQL bağlantısı başarıyla gerçekleştirildi.');
-
+connection.query("select user()",(err,result)=>{
+    console.log(result);
 })
-
 
